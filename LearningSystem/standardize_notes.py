cf622a85c6d694e21328389394714115ee40f64d
Suyash Rahegaonkar <rsuyash123@gmail.com>
2025-03-27 19:31:05 +0530

    Standardized note properties

--- LearningSystem/standardize_notes.py
+++ LearningSystem/standardize_notes.py
@@ -57,6 +57,9 @@ def standardize_notes(vault_path):
             # Remove non-alphanumeric characters from lines starting with "Title:", "Definition:", "Tags:", "Products:", "Related:", "Parent:"
             frontmatter_str = re.sub(r"(^|\n)(Title:|Definition:|Tags:|Products:|Related:|Parent:)\s*[^\w\s\[\],#]+", r"\1\2 ", frontmatter_str)
 
+            # Remove non-alphanumeric characters from lines starting with "Title:", "Definition:", "Tags:", "Products:", "Related:", "Parent:", "Status:", "Purpose:"
+            frontmatter_str = re.sub(r"(^|\n)(Title:|Definition:|Tags:|Products:|Related:|Parent:|Status:|Purpose:)\s*[^\w\s\[\],#]+", r"\1\2 ", frontmatter_str)
+
             # Load the frontmatter using yaml
             try:
                 metadata = yaml.safe_load(frontmatter_str) if frontmatter_str else {}
