dfdbca9d5aa50cc81982d2b55b7669c68f8e3302
Suyash Rahegaonkar <rsuyash123@gmail.com>
2025-03-27 19:30:29 +0530

    Standardized note properties

--- LearningSystem/standardize_notes.py
+++ LearningSystem/standardize_notes.py
@@ -51,11 +51,11 @@ def standardize_notes(vault_path):
             # Remove non-alphanumeric characters from lines starting with "Related:", "Parent:"
             frontmatter_str = re.sub(r"(^|\n)(Related:|Parent:)\s*[^\w\s\[\],#]+", r"\1\2 ", frontmatter_str)
 
-            # Remove non-alphanumeric characters from lines starting with "Title:", "Definition:"
-            frontmatter_str = re.sub(r"(^|\n)(Title:|Definition:)\s*[^\w\s:]+", r"\1\2 ", frontmatter_str)
-
             # Remove non-alphanumeric characters from lines starting with "Title:", "Definition:", "Tags:", "Products:", "Related:", "Parent:"
             frontmatter_str = re.sub(r"(^|\n)(Title:|Definition:|Tags:|Products:|Related:|Parent:)\s*[^\w\s:]+", r"\1\2 ", frontmatter_str)
+
+            # Remove non-alphanumeric characters from lines starting with "Title:", "Definition:", "Tags:", "Products:", "Related:", "Parent:"
+            frontmatter_str = re.sub(r"(^|\n)(Title:|Definition:|Tags:|Products:|Related:|Parent:)\s*[^\w\s\[\],#]+", r"\1\2 ", frontmatter_str)
 
             # Load the frontmatter using yaml
             try:
